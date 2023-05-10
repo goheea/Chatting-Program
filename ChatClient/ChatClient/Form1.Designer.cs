fc4141e32cd3d1461699526df348ae9e4f46f720
HEEA <58849237+goheea@users.noreply.github.com>
2023-05-10 10:22:12 +0900

    Add files via upload

---
+++ ChatClient/ChatClient/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Logout = new System.Windows.Forms.Button();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ServerIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rt_Message = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_Logout);
+            this.panel1.Controls.Add(this.btn_Login);
+            this.panel1.Controls.Add(this.txt_user);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txt_Port);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txt_ServerIP);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(746, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_Logout
+            // 
+            this.btn_Logout.Location = new System.Drawing.Point(508, 21);
+            this.btn_Logout.Name = "btn_Logout";
+            this.btn_Logout.Size = new System.Drawing.Size(141, 62);
+            this.btn_Logout.TabIndex = 7;
+            this.btn_Logout.Text = "종료";
+            this.btn_Logout.UseVisualStyleBackColor = true;
+            this.btn_Logout.Click += new System.EventHandler(this.btn_Logout_Click);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(350, 21);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(141, 62);
+            this.btn_Login.TabIndex = 6;
+            this.btn_Login.Text = "로그인";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(89, 58);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(100, 25);
+            this.txt_user.TabIndex = 5;
+            this.txt_user.Text = "김지원";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "사용자명";
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(258, 18);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(71, 25);
+            this.txt_Port.TabIndex = 3;
+            this.txt_Port.Text = "7000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "포트";
+            // 
+            // txt_ServerIP
+            // 
+            this.txt_ServerIP.Location = new System.Drawing.Point(89, 18);
+            this.txt_ServerIP.Name = "txt_ServerIP";
+            this.txt_ServerIP.Size = new System.Drawing.Size(100, 25);
+            this.txt_ServerIP.TabIndex = 1;
+            this.txt_ServerIP.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "서버IP";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btn_Send);
+            this.panel2.Controls.Add(this.txt_message);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 395);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(746, 55);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(438, 15);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(98, 30);
+            this.btn_Send.TabIndex = 7;
+            this.btn_Send.Text = "보내기";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(68, 15);
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(364, 25);
+            this.txt_message.TabIndex = 3;
+            this.txt_message.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_message_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "메시지";
+            // 
+            // rt_Message
+            // 
+            this.rt_Message.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rt_Message.Location = new System.Drawing.Point(0, 100);
+            this.rt_Message.Name = "rt_Message";
+            this.rt_Message.Size = new System.Drawing.Size(746, 295);
+            this.rt_Message.TabIndex = 2;
+            this.rt_Message.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 450);
+            this.Controls.Add(this.rt_Message);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ServerIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rt_Message;
+        private System.Windows.Forms.Button btn_Logout;
+    }
+}
+
---
+++ MultiChatServer/MultiChatServer/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace MultiChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_ServerPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Message = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txt_ServerPort);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "연결대기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_ServerPort
+            // 
+            this.txt_ServerPort.Location = new System.Drawing.Point(116, 10);
+            this.txt_ServerPort.Name = "txt_ServerPort";
+            this.txt_ServerPort.Size = new System.Drawing.Size(100, 25);
+            this.txt_ServerPort.TabIndex = 1;
+            this.txt_ServerPort.Text = "7000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "접속대기포트";
+            // 
+            // txt_Message
+            // 
+            this.txt_Message.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_Message.Location = new System.Drawing.Point(0, 47);
+            this.txt_Message.Name = "txt_Message";
+            this.txt_Message.Size = new System.Drawing.Size(800, 403);
+            this.txt_Message.TabIndex = 3;
+            this.txt_Message.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_Message);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_ServerPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txt_Message;
+    }
+}
+
